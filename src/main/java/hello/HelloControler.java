ae81420c3d1e962434b5cb296ca77675c464fc07
lumba <matej.balun@fer.hr>
2017-07-26 16:25:52 +0200

    "Tests not running properly"

--- src/main/java/hello/HelloControler.java
+++ src/main/java/hello/HelloControler.java
@@ -1,7 +1,15 @@
 package hello;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by lumba on 26.07.17..
  */
 public class HelloControler {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 }
